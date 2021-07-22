6c74b3f0385095529d444055a1a57d9c313c2a0f
CZJ <followmytype@gmail.com>
2021-07-22 21:43:01 +0800

    fix: 修改fun名稱

--- src/Pack/IntPack.php
+++ src/Pack/IntPack.php
@@ -8,25 +8,25 @@ class IntPack
     public function pack(int $int) 
     {
         if ($int < 128) {
-            $hex = $this->addZero($int, 2);
+            $hex = $this->leftAddZero($int, 2);
         } elseif ($int < 256) {
-            $hex = $this->addZero($int, 2);
+            $hex = $this->leftAddZero($int, 2);
             $hex = dechex(Spec::$int8) . $hex;
         } elseif ($int < 65536) {
-            $hex = $this->addZero($int, 4);
+            $hex = $this->leftAddZero($int, 4);
             $hex = dechex(Spec::$int16) . $hex;
         } elseif ($int < 4294967296) {
-            $hex = $this->addZero($int, 8);
+            $hex = $this->leftAddZero($int, 8);
             $hex = dechex(Spec::$int32) . $hex;
         } elseif ($int < pow(2, 64)) {
-            $hex = $this->addZero($int, 16);
+            $hex = $this->leftAddZero($int, 16);
             $hex = dechex(Spec::$int64) . $hex;
         }
 
         return hex2bin($hex);
     }
 
-    private function addZero(int $int, int $num)
+    private function leftAddZero(int $int, int $num)
     {
         return sprintf("%0" . $num . "x", $int);
     }
